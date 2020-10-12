e4fd6ae542df69593c99e07a9bfd6f0f507cdc2b
Antoine Genonceau <antoine.genonceau@isen.yncrea.fr>
2020-10-12 16:36:44 +0200

    paramètres Case

--- ProjetConcept/src/projetconcept/Case.java
+++ ProjetConcept/src/projetconcept/Case.java
@@ -11,8 +11,49 @@ package projetconcept;
  */
 public class Case {
     
+    private int x;
+    private int y;
+    
+    private Object contenu;
+    
     public Case(){
                
+        
+    }
+        
+    public int getX(){
+        
+        return this.x;
+        
+    }
+    
+    public int getY(){
+        
+        return this.y;
+        
+    }
+    
+    public Object getContenu(){
+        
+        return this.contenu;
+        
+    }
+    
+    public void setX(int pX){
+        
+        this.x = pX;
+        
+    }
+    
+    public void setY(int pY){
+        
+        this.y = pY;
+        
+    }
+    
+    public void setContenu(Object pContenu){
+        
+        this.contenu = pContenu;
         
     }
     
